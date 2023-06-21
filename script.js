5a6deee41a8c48cc0f1acc5effcb51771fd866de
Anitha chowdary <130220172+anitha-anii@users.noreply.github.com>
2023-06-21 17:13:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
-//your JS code here. If required.
-let buttons = document.quarySelector('buttons')[0];
+
+  let buttons = document.querySelectorAll('button');
+  let display = document.getElementById('display');
+
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', function() {
+      if (this.innerHTML === '=') {
+        display.innerText = eval(display.innerText);
+      } else if (this.innerHTML === 'C') {
+        display.innerText = '';
+      } else {
+        display.innerText += this.innerHTML;
+      }
+    });
+  }
+
+
