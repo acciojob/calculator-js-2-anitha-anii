d28970ab70923dcf2cfc6e179a6c5103227ef854
Anitha chowdary <130220172+anitha-anii@users.noreply.github.com>
2023-06-21 17:17:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
 
-  let buttons = document.querySelectorAll('button');
-  let display = document.getElementById('display');
+document.addEventListener("DOMContentLoaded", function() {
+  const display = document.getElementById("display");
+  const buttons = document.querySelectorAll("button");
 
-  for (let i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener('click', function() {
-      if (this.innerHTML === '=') {
-        display.innerText = eval(display.innerText);
-      } else if (this.innerHTML === 'C') {
-        display.innerText = '';
+  buttons.forEach(button => {
+    button.addEventListener("click", function() {
+      const value = button.innerText;
+      const currentDisplay = display.value;
+
+      if (value === "C") {
+        display.value = "";
+      } else if (value === "&larr;") {
+        display.value = currentDisplay.slice(0, -1);
+      } else if (value === "=") {
+        try {
+          display.value = eval(currentDisplay);
+        } catch (error) {
+          display.value = "Error";
+        }
       } else {
-        display.innerText += this.innerHTML;
+        display.value += value;
       }
     });
-  }
+  });
+});
+
 
 
